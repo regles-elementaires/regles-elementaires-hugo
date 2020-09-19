1d369715105728780def63a8fb498e43049ce8fd
NicolasDutronc <nicolas.dutronc@orange.fr>
2020-09-19 19:01:06 +0200

    added popup style and js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ import "./css/main.css";
 
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
+
+var popup = document.getElementById("popup");
+var close = document.getElementById("close");
+close.addEventListener('click', closePopup);
+
+function closePopup() {
+    popup.style.display = 'none';
+}
