47b18c8d4e2cd8c682d80eea5aaae14650f7f362
NicolasDutronc <nicolas.dutronc@orange.fr>
2020-09-30 21:45:38 +0200

    renamed popup div to overlay clicking outside the popup now closes it

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import "./css/main.css";
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
 
-var popup = document.getElementById("popup");
+var overlay = document.getElementById("overlay");
 var close = document.getElementById("close");
 close.addEventListener('click', closePopup);
+overlay.addEventListener('click', closePopup)
 
-function closePopup() {
-    popup.style.display = 'none';
+function closePopup(ev) {
+    if (ev.target === overlay || ev.target === close) {
+        overlay.style.display = 'none';
+    }
 }
