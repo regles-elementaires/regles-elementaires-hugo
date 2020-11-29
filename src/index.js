cdfb722136da5c4000818e239acd5ae7ca88eeec
NicolasDutronc <nicolas@dansmaculotte.com>
2020-11-29 18:08:13 +0100

    changed background color css class for popups overlays

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ console.log("🦊 Hello! Edit me in src/index.js");
 
 var overlay = document.getElementById("overlay");
 var close = document.getElementById("close");
-close.addEventListener('click', closePopup);
-overlay.addEventListener('click', closePopup)
+if (close !== null) {
+    close.addEventListener('click', closePopup);
+}
+if (overlay !== null) {
+    overlay.addEventListener('click', closePopup)
+}
 
 function closePopup(ev) {
     if (ev.target === overlay || ev.target === close) {
