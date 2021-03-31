4ebf703d4db7ea09be1c7161779e591fecf76abb
bastien-germain <bastiengermain1708@hotmail.fr>
2021-03-31 15:29:46 +0200

    netlify : switch to yarn again

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const merge = require("webpack-merge");
 const TerserPlugin = require("terser-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
@@ -21,13 +20,6 @@ module.exports = merge(common, {
         parallel: true,
         sourceMap: true,
         exclude: /\/node_modules\//,
-      }),
-      new CleanWebpackPlugin({
-        cleanOnceBeforeBuildPatterns: [
-          "dist/**/*.js",
-          "dist/**/*.css",
-          "site/data/webpack.json"
-        ]
       }),
       new MiniCssExtractPlugin({
         filename: "[name].[hash:5].css",
