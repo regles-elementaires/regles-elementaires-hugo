f27be650bb3f4bde9bd93b97bbac67d27020c882
bastien-germain <bastiengermain1708@hotmail.fr>
2021-06-24 10:17:55 +0200

    new popup

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import "./css/main.css";
 
 var overlay = document.getElementById("overlay");
 var close = document.getElementById("close");
+var closeButton = document.getElementById("close-button");
 if (close !== null) {
     close.addEventListener('click', closePopup);
 }
 if (overlay !== null) {
     overlay.addEventListener('click', closePopup)
 }
+if (closeButton !== null) {
+    closeButton.addEventListener('click', closePopup);
+}
 
 function closePopup(ev) {
-    if (ev.target === overlay || ev.target === close) {
+    console.log(ev.currentTarget);
+    if (ev.target === overlay || ev.target === close || ev.currentTarget === closeButton) {
         overlay.style.display = 'none';
     }
 }
