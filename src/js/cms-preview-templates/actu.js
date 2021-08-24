4713d18d848b9ad787ae8e01b67efc3c1b1e2dde
bastien-germain <bastiengermain1708@hotmail.fr>
2021-08-24 09:49:38 +0200

    histoire collecte : layout & admin

--- src/js/cms-preview-templates/actu.js
+++ src/js/cms-preview-templates/actu.js
@@ -17,37 +17,49 @@ const TagEntries = ({data}) => data && data.length > 0
 
 export default class ActuPreview extends React.Component {
   render() {
-    const {entry, widgetFor, getAsset} = this.props;
-    let illu = getAsset(entry.getIn(["data", "illu"]));
+    const {entry, getAsset} = this.props;
+    const illu = getAsset(entry.getIn(["data", "illu"]));
+    const logo = getAsset(entry.getIn(["data", "logo"]));
     const entryTagEntries = entry.getIn(["data", "tags"]);
     const tagEntries = entryTagEntries ? entryTagEntries.toJS() : [];
-
-    return <div> 
-    <div className="w-11/12 lg:w-4/6 m-auto pt-8 pb-4 flex flex-row px-2 justify-center tracking-tighter">
-        <div className="flex flex-col md:flex-row w-full items-center">
-            <div className="w-full max-w-xs md:max-w-sm h-full">{ illu && <img className="object-contain object-top w-full h-full" src={ illu } /> }</div>
-            <div className="w-full md:pl-4">
-                <h2 className="mt-4 md:mt-0 text-2xl md:text-3xl font-bold uppercase text-redarkgrey">
-                    { entry.getIn(["data", "title"]) }
-                </h2>
-                <p className="my-4">
-                    {/*  Date format ? */}
-                    <span className="text-rered">le { format(entry.getIn(["data", "date"]), "DD.MM.YYYY") }</span>
-                </p>
-                <TagEntries data={tagEntries} />
+    let logoDiv;
+    if (logo) {
+        logoDiv = (
+            <div class="w-24 h-24">
+                <a href={ entry.getIn(["data", "link"]) } target="_blank" class="w-24 h-24">
+                    <img src={ logo } class="object-contain rounded-full border-rebeige border-4 w-24 h-24" />
+                </a>
             </div>
-        </div>
-    </div>
-    <div className="w-11/12 lg:w-4/6 m-auto text-rered mt-2 pb-8 px-2 justify-center flex flex-row flex-wrap">
-        <span class="w-full border-t-4 border-rebeige mb-4 md:mb-8 md:mt-4"></span>
-        <div className="w-full mb-8 text-rered text-xl md:text-2xl markdown tracking-tighter">
-            <ReactMarkdown>{ entry.getIn(["data", "intro"]) }</ReactMarkdown>
-        </div>
-        <div className="w-full markdown text-redarkgrey text-md md:text-lg font-medium tracking-tighter">
-            <ReactMarkdown>{ entry.getIn(["data", "body"]) }</ReactMarkdown>
-        </div>
-    </div> 
-    </div>;
+        );
+    }
 
+    return (
+        <div> 
+            <div className="w-11/12 lg:w-4/6 m-auto pt-8 pb-4 flex flex-row px-2 justify-center tracking-tighter">
+                <div className="flex flex-col md:flex-row w-full items-center">
+                    <div className="w-full max-w-xs md:max-w-sm h-full rounded-lg">{ illu && <img className="rounded-lg object-contain object-top w-full" src={ illu } /> }</div>
+                    <div className="w-full md:pl-4">
+                        <h2 className="mt-4 md:mt-0 text-2xl md:text-3xl font-bold uppercase text-redarkgrey">
+                            { entry.getIn(["data", "title"]) }
+                        </h2>
+                        <p className="my-4">
+                            <span className="text-rered">le { format(entry.getIn(["data", "date"]), "DD.MM.YYYY") }</span>
+                        </p>
+                        <TagEntries data={tagEntries} />
+                        { logoDiv }
+                    </div>
+                </div>
+            </div>
+            <div className="w-11/12 lg:w-4/6 m-auto text-rered mt-2 pb-8 px-2 justify-center flex flex-row flex-wrap">
+                <span class="w-full border-t-4 border-rebeige mb-4 md:mb-8 md:mt-4"></span>
+                <div className="w-full mb-8 text-rered text-xl md:text-2xl markdown tracking-tighter">
+                    <ReactMarkdown>{ entry.getIn(["data", "intro"]) }</ReactMarkdown>
+                </div>
+                <div className="w-full markdown text-redarkgrey text-md md:text-lg font-medium tracking-tighter">
+                    <ReactMarkdown>{ entry.getIn(["data", "body"]) }</ReactMarkdown>
+                </div>
+            </div> 
+        </div>
+    );
   }
 }
